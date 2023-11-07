77fa1681098c5d93e34a69acfba1f5589a7102ed
MarselGn <marsel69.ru@mail.ru>
2023-11-07 20:42:29 +0500

    add func

--- superjob.py
+++ superjob.py
@@ -1,26 +1,48 @@
 import os
 import requests
-import json
 from api_abc import API
 
 
 class SuperJobAPI(API):
-    __API_KEY: str = os.getenv('API_KEY_SJ')
-    __url = 'https://api.superjob.ru/2.0/vacancies/'
+    """Класс для работы с API сайта с вакансиями: superjob.ru """
 
-    def get_vacancies(self, keyword) -> list:
-        req = requests.get(self.__url,
-                           headers={"X-Api-App-Id": self.__API_KEY},
-                           params={'keywords': keyword, 'page': 0, 'count': 100}).json()
-        list_vacancies = []
-        for i in req['objects']:
-            dict_vacancies = {'vacancy_name': i['profession'],
-                              'vacancy_url': i['link'],
-                              'salary': f'{i['payment_from']-i['payment_to']}',
-                              'vacancy_responsibilities': i['candidat']}
-            list_vacancies.append(dict_vacancies)
-        return list_vacancies
+    def __init__(self):
+        """Создание экземпляра класса SuperJobAPI"""
+        self.url = "https://api.superjob.ru/2.0/vacancies/"
+        self.headers = {
+            'X-Api-App-Id': os.getenv('API_KEY'),
+        }
 
+    def get_vacancies(self, keyword) -> dict:
+        """Возвращает отфильтрованные по ключевому слову вакансии с сайта """
 
-sj = SuperJobAPI()
-print(sj.get_vacancies("python"))
+        params = {
+            "keyword": keyword,  # ключевое слово фильтра
+            "town": 4,  # номер региона (4 - Москва)
+            "count": 50,  # количество вакансий
+        }
+        response = requests.get(self.url, headers=self.headers, params=params)
+        if response.status_code == 200:
+            raise Exception(f"Request failed with status code: {response.status_code}")
+        else:
+            data = response.json()
+            return data
+
+    @staticmethod
+    def clean_vacancies(data) -> list:
+        """Приводит данные к единому стандарту """
+
+        clean_vacancies = []
+        vacancies = data.get("objects", [])
+        for vacancy in vacancies:
+            vacancy_title = vacancy.get("profession")
+            vacancy_url = vacancy.get("link")
+            vacancy_salary_from = vacancy.get("payment_from")
+            vacancy_employer = vacancy.get("client", {}).get("title")
+
+            clean_vacancies.append({"title": vacancy_title,
+                                    "url": vacancy_url,
+                                    "salary_from": vacancy_salary_from,
+                                    "employer": vacancy_employer})
+
+        return clean_vacancies
